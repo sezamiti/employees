8921f02d423f709acaff3c39a58d9c6c78a9ac9a
Sezam <dossumbekov@list.ru>
2022-07-11 03:13:26 +0600

    fix: Responsive style v3

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -26,31 +26,34 @@ const EmployeesListItem = (props) => {
               {name}
             </span>
           </Col>
+          <Col>
+            <input
+              type="text"
+              className="list-group-item-input"
+              defaultValue={salary + "$"}
+            />
+          </Col>
+          <Col>
+            <div className="d-flex justify-content-center align-items-center">
+              <button
+                type="button"
+                className="btn-cookie btn-sm "
+                onClick={onToggleProp}
+                data-toggle="increase"
+              >
+                <i className="fas fa-cookie"></i>
+              </button>
 
-          <input
-            type="text"
-            className="list-group-item-input"
-            defaultValue={salary + "$"}
-          />
-          <div className="d-flex justify-content-center align-items-center">
-            <button
-              type="button"
-              className="btn-cookie btn-sm "
-              onClick={onToggleProp}
-              data-toggle="increase"
-            >
-              <i className="fas fa-cookie"></i>
-            </button>
-
-            <button
-              type="button"
-              className="btn-trash btn-sm "
-              onClick={onDelete}
-            >
-              <i className="fas fa-trash"></i>
-            </button>
-            <i className="fas fa-star"></i>
-          </div>
+              <button
+                type="button"
+                className="btn-trash btn-sm "
+                onClick={onDelete}
+              >
+                <i className="fas fa-trash"></i>
+              </button>
+              <i className="fas fa-star"></i>
+            </div>
+          </Col>
         </li>
       </Row>
     </Container>
